d16abbce4bcdf49cc0a69d5883e61eda9a5ef252
Pratyush Desai <pratyush.desai@liberta.casa>
2021-10-30 11:59:07 +0530

    flask library speedrun

---
+++ flaskr/__init__.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_marshmallow import Marshmallow
+
+# NOTE:
+# Deciding to go stepwise instead of jumping around
+# https://flask.palletsprojects.com/en/2.0.x/tutorial/factory/
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+ 
+
+
+# app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+
+
+# db = SQLAlchemy(app)
+# ma = Marshmallow(app)
+
+
+
+
+
+# Ends with:
+# if __name__ == "__main__":
+#     app.run(debug=True)
